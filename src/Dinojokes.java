54358ac6b1b0360849b60a4bb59bcb3f556dd9b9
Vince <vince@krijnsen.com>
2023-09-14 08:01:24 +0100

    Fix: var names and missing exclamation point

--- src/Dinojokes.java
+++ src/Dinojokes.java
@@ -4,12 +4,12 @@ public class Dinojokes {
     public static void main(String[] args) {
         String banner = "===============\n== DINOJOKES ==\n===============\n";
         System.out.println(banner);
-        jokeloop();
+        jokeLoop();
         System.exit(0);
     }
 
-    private static void jokeloop() {
-        String askInput = "Press enter to see another dinojoke, or 'q' then enter to quit: ";
+    private static void jokeLoop() {
+        String askInput = "Press enter to see another dino joke, or 'q' then enter to quit: ";
         Scanner in = new Scanner(System.in);
         String userInput;
         do {
@@ -21,15 +21,16 @@ public class Dinojokes {
     }
 
     private static String randomJoke() {
+        List<String> jokeList = Dinojokes.hilarity;
         Random random = new Random();
-        int randomIndex = random.nextInt(Dinojokes.hilarity.size());
-        return Dinojokes.hilarity.get(randomIndex) + "\n";
+        int randomIndex = random.nextInt(jokeList.size());
+        return jokeList.get(randomIndex) + "\n";
     }
 
     private static final List<String> hilarity = new ArrayList<>(Arrays.asList(
             "What do you call a dinosaur that buzzes?\nA BEE-REX!",
             "What do you call a sleeping dinosaur?\nA DINO-SNORE!",
-            "What do you call a laughing dinosaur?\nA GIGGLE-NOTOSAURUS"
+            "What do you call a laughing dinosaur?\nA GIGGLE-NOTOSAURUS!"
     ));
 
 }
